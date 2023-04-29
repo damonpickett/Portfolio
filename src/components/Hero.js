b0fc56a7996442396eafac87b2b9d04b7b344b2c
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2023-04-28 20:59:03 -0700

    adjusted hero for smaller screens

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,20 +11,20 @@ const Hero = (props) => {
     <section className="hero" data-aos='fade-right'>
 
       <div className='hero__content'>
-      <div className="hero__text">
-        <h1>
-          Hi, I'm <span className="camelCase">damonPickett</span>
-        </h1>
-        <h2>Software Developer</h2>
-          <button onClick={() => props.setShow(true)} className="button">Get In Touch</button>
-      </div>
-
-      <div className='image__content'>
-        <div className='image-container'>
-          <img src={cyberProfile} alt="Damon Pickett" />
-          <img src={humanProfile} alt="Damon Pickett" />
+        <div className="hero__text">
+          <h1>
+            Hi, I'm <span className="camelCase">damonPickett</span>
+          </h1>
+          <h2>Software Developer</h2>
+            <button onClick={() => props.setShow(true)} className="button">Get In Touch</button>
         </div>
-      </div>
+
+        <div className='image__content'>
+          <div className='image-container'>
+            <img src={cyberProfile} alt="Damon Pickett" />
+            <img src={humanProfile} alt="Damon Pickett" className='real-me'/>
+          </div>
+        </div>
       </div>
 
     </section>
